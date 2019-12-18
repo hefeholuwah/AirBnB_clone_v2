d7d3fbd18c695ec123c0a333b989758ad095f3d4
Miuywu <859@holbertonschool.com>
2019-12-18 23:05:01 +0000

    task 7

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """This is the file storage class for AirBnB"""
 from models.base_model import BaseModel, Base
-# from models.user import User
+from models.user import User
 from models.state import State
 from models.city import City
 # from models.amenity import Amenity
