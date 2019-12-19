9a34649c9b34b333de73686bd8d0ca58f110a7dc
Miuywu <859@holbertonschool.com>
2019-12-19 01:55:49 +0000

    Revert "task 8"
    This reverts commit b2cbbdf841a399aababf8b383f8fb86fdc6317bd.

--- models/place.py
+++ models/place.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 """This is the place class"""
 from models.base_model import BaseModel
-from sqlalchemy import Column, Interger, String, ForeignKey
-from sqlalchemy.orm import relationship
 
 
 class Place(BaseModel):
@@ -20,60 +18,14 @@ class Place(BaseModel):
         longitude: longitude in float
         amenity_ids: list of Amenity ids
     """
-    __tablename__ = "places"
-    city_id = Column(String(60),
-                     nullable=False,
-                     ForeignKey("cities.id"))
-
-    user_id = Column(String(60),
-                     nullable=False,
-                     ForeignKey("user.id"))
-
-    name = Column(String(128),
-                  nullable=False)
-
-    description = Column(String(1024),
-                         nullable=True)
-
-    number_rooms = Column(Integer,
-                          nullable=False,
-                          default=0)
-
-    number_bathrooms = Column(Integer,
-                              nullable=False,
-                              default=0)
-
-    max_guest = Column(Integer,
-                       nullable=False,
-                       default=0)
-
-    price_by_night = Column(Integer,
-                            nullable=False,
-                            default=0)
-
-    latitude = Column(Float,
-                      nullable=True)
-    longitude = Column(Float,
-                       nullable=True)
+    city_id = ""
+    user_id = ""
+    name = ""
+    description = ""
+    number_rooms = 0
+    number_bathrooms = 0
+    max_guest = 0
+    price_by_night = 0
+    latitude = 0.0
+    longitude = 0.0
     amenity_ids = []
-
-    """ CHILD OF USER """
-    """ user = relationship("User","""
-    """              backref=backref("places", cascade="all, delete-orphan"))"""
-    """ CHILD OF CITIES"""
-    """cities = relationship("City", """
-    """                      backref=backref, cascade="all, delete-orphan"))"""
-    """ place_amenity = Table("""
-    """    "place_amenity","""
-    """    Base.metadata,"""
-    """    Column('place_id',"""
-    """        String(60),"""
-    """     primary_key=True,"""
-    """           ForeignKey("place.id"),"""
-    """           nullable=False),"""
-    """    Column('amenity_id',"""
-    """           String(60),"""
-    """           primary_key=True,"""
-    """           ForeignKey("amenty.id"),"""
-    """           nullable=False)"""
-    """)"""
