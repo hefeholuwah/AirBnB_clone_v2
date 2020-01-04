7e6325ebe640a6bebd8dc588ce37fbe5a27909b8
Miuywu <859@holbertonschool.com>
2020-01-04 05:13:13 +0000

    task 0

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# config script
+apt-get -y update
+apt-get -y install nginx
+
+ufw allow 'Nginx HTTP'
+mkdir -p /data/web_static/releases/test
+mkdir -p /data/web_static/shared
+echo "test" > /data/web_static/releases/test/index.html
+
+ln -sf /data/web_static/releases/test /data/web_static/current
+chown -R ubuntu:ubuntu /data
+sed -i '/listen 80 default_server;/a location /hbnb_static/ { alias /data/web_static/current/; }' /etc/nginx/sites-available/default
+
+service nginx restart
