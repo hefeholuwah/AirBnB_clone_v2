4283fded11b544f4d8e47f9f91aa51a6645effae
Miuywu <859@holbertonschool.com>
2020-01-23 03:17:55 +0000

    removed if

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -39,9 +39,8 @@ def number_route(n):
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
-    """Display web page if type(n) == int"""
-    if type(n) == int:
-        return render_template('5-number.html', n=n)
+    """Display web page"""
+    return render_template('5-number.html', n=n)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
