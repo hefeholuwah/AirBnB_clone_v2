c65c9f4d89447b5f0a1883c414a7fcfac39f7b8e
root <ilieas300@gmail.com>
2023-05-08 00:29:54 +0000

    change web static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,20 +1,25 @@
 #!/usr/bin/env bash
 
-# Update package lists and install Nginx
-sudo apt-get update
-sudo apt-get install -y nginx
+# Install Nginx if not already installed
+if [ $(dpkg-query -W -f='${Status}' nginx 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
+    sudo apt-get -y update
+    sudo apt-get -y install nginx
+fi
 
-# Allow HTTP traffic through UFW firewall
-sudo ufw allow 'Nginx HTTP'
+# Create necessary directories
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared /data/web_static/releases
 
-# Create directories for static files and set up test file
-sudo mkdir -p /data/web_static/releases/test
-sudo chown -R $USER:$USER /data/web_static/shared
-echo "This is a test page" | sudo tee /data/web_static/releases/test/index.html
+# Create a fake HTML file for testing
+sudo echo "Fake HTML file" | sudo tee /data/web_static/releases/test/index.html
 
-# Set up Nginx virtual host for static files
-sudo cp /data/web_static/releases/test /data/web_static/current
+# Create symbolic link
+sudo ln -sf /data/web_static/releases/test /data/web_static/current
+
+# Give ownership of /data/ folder to ubuntu user and group
+sudo chown -R ubuntu:ubuntu /data/
+
+# Update Nginx configuration
 sudo sed -i '/listen 80 default_server;/a location /hbnb_static/ { alias /data/web_static/current/; }' /etc/nginx/sites-available/default
 
 # Restart Nginx
-sudo systemctl restart nginx
+sudo service nginx restart
